b21091a0fc9c4c510a7b6aad37d8dcdb77a8687e
Pablitah <pablitahmontoya@outlook.com>
2026-04-02 13:23:19 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,22 +3,24 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 
-# Project info
+# Project information
 project = 'PCM Matic'
-author = 'Shan Goshorn'
+author = 'Rose Gomes'
 release = '1.0'
 
-# General config
-extensions = []
+# General configuration
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# HTML settings
+# HTML output
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# ✅ Google Verification (WORKING METHOD)
-html_context = {
-    "<meta name="google-site-verification" content="xMvhEb7ayr2oSB67CmQJ-XqQ7Pati-Stu9KHRdN-J6c" />" />'
+# Google site verification (FIXED)
+html_meta = {
+    "google-site-verification": "xMvhEb7ayr2oSB67CmQJ-XqQ7Pati-Stu9KHRdN-J6c"
 }
